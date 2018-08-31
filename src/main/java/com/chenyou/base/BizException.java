58c0d04786c83a4a5621a3d42defb15316bcd6ad
HaoLinXin <haowenjie7173@163.com>
2018-08-31 21:38:21 +0800

    接口请求方式添加，service测试，dao测试

--- src/main/java/com/chenyou/base/BizException.java
+++ src/main/java/com/chenyou/base/BizException.java
@@ -12,7 +12,8 @@ public class BizException extends Exception {
     public static final String CODE_PARM_LACK        = "BASE-1002";       //缺少必要参数
     public static final String CODE_PARM_ERROR       = "BASE-1003";       //参数错误
     public static final String CALL_SERVICE_ERROR    = "BASE-1004";       //调用服务异常
-    
+
+    //push
     private String code;
     public BizException(String code, String message) {
         super(message);
